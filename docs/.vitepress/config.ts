969619f92c2755de150aeec6a0c23cdd62bb8a43
pkc918 <xiaopan2000918@gmail.com>
2024-05-15 00:10:59 +0800

    chore: modify code style to the same with source code

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -304,16 +304,16 @@ export default defineConfig({
       options: {
         translations: {
           button: {
-            buttonText: "搜索文档",
-            buttonAriaLabel: "搜索文档"
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
           },
           modal: {
-            noResultsText: "无法找到相关结果",
-            resetButtonTitle: "清除查询条件",
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
             footer: {
-              selectText: "选择",
-              navigateText: "切换",
-              closeText:"关闭"
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
             }
           }
         }
